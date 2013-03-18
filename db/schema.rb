e05d37818e730771e57bfb7bff8ceee9e23da9f8
kaka <houlizhi1111@sina.com>
2013-03-18 15:14:01 +0800

    change password_digest to crypted_password

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315130022) do
+ActiveRecord::Schema.define(:version => 20130318070315) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.string   "remember_token"
+    t.string   "crypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
