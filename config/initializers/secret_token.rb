3073447a663bce5f9a4070750c04f98f3d4bfb8d
kaka <houlizhi1111@sina.com>
2013-03-18 11:51:03 +0800

    register login logout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserManagement::Application.config.secret_token = 'bac91a328b87f067c24f0cdf05606954edaae93b46a88d40ffd0456559c3f8deba243079c4899a1ffb8b0e200651a411d23fbe226335385b6eed308973cae00d'
